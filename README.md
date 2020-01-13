eaab1886ec209f8e9754f87a4bf9359db7dff917
jack <191893675@qq.com>
2020-01-13 12:45:18 +0800

    date-pickers

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # iview-extend-datepicker
-一个基于iview的日期组件扩展的日期组件
+一个基于iview的日期组件扩展的日期组件, 基于iview 2.0版本
+
+> 增加：季度选择 type=“quarter”
+> 季度范围选择 type=“quarterrange”
+> 月份范围选择 type=“monthrange”
+>
+> 若版本大于2.0 注意icon图标 date-pickers/picker.vue => 注意:239行方法iconType()
+
+- 下载日期组件`date-pickers`将其放到compononents目录下，当然也可以放到其他目录。
+
+- ```javascript
+  // 引入组件，注册组件
+  import DatePickers from '../../components/date-pickers'
+  components: { DatePickers }
+  ```
+
+- ```html
+  // 使用组件，使用方法可参考demo.vue
+  <DatePickers type="monthrange" transfer format="yyyy-MM" v-model="month" placeholder="选择月份区间" style="width: 200px"></DatePickers>
+  
+  <DatePickers type="quarterrange" transfer format="yyyy-MM-dd" v-model="season" placeholder="选择季节区间" style="width: 200px"></DatePickers>
+  ```
+
+- type类型包括: [‘year’, ‘month’, ‘date’, ‘quarter’, ‘daterange’, ‘datetime’, ‘datetimerange’,‘monthrange’,‘quarterrange’]
+  其他具体参数与iview相同
---
+++ date-pickers/panel/Date/date-panel-label.vue
@@ -0,0 +1,28 @@
+<template>
+    <span>
+        <span
+            v-if="datePanelLabel"
+            v-show="datePanelLabel.labels[0].type === 'year' || currentView === 'date'"
+            :class="[datePrefixCls + '-header-label']"
+            @click="datePanelLabel.labels[0].handler">{{ datePanelLabel.labels[0].label }}</span>
+        <template v-if="datePanelLabel && currentView === 'date'">{{ datePanelLabel.separator }}</template>
+        <span
+            v-if="datePanelLabel&&type=='date'"
+            v-show="datePanelLabel.labels[1].type === 'year' || currentView === 'date'"
+            :class="[datePrefixCls + '-header-label']"
+            @click="datePanelLabel.labels[1].handler">{{ datePanelLabel.labels[1].label }}</span>
+    </span>
+</template>
+
+<script>
+  console.log(this.datePanelLabel);
+  export default {
+    props: {
+        datePanelLabel: Object,
+        currentView: String,
+        datePrefixCls: String,
+        type:String
+    },
+
+};
+</script>
